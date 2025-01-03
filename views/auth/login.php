5ecb05d9847a20ff53b1785de67c3485ccfa6119
Leonardo_Parhikuni <leonardohuape@gmail.com>
2025-01-03 15:27:29 -0600

    Creando el Login

---
+++ views/auth/login.php
@@ -0,0 +1,27 @@
+<main class="auth">
+    <h2 class="auth__titulo">DevWebCamp</h2>
+    <p class="auth__descripcion"> Inicia Session o crea una cuenta</p>
+
+    <form class="formulario">
+        <div class="formulario__campo">
+            <label for="email" class="formulario__label">Correo</label>
+            <input 
+                type="email" 
+                id="email" 
+                name="email" 
+                class="formulario__input" 
+                placeholder="Tu Correo"/>
+        </div>
+        <div class="formulario__campo">
+            <label for="password" class="formulario__label">Password</label>
+            <input 
+                type="password" 
+                id="password" 
+                name="password" 
+                class="formulario__input" 
+                placeholder="Tu password"/>
+        </div>
+        <input type="submit" value="Iniciar Sesión" class="formulario__submit"> 
+    </form>
+
+</main>
